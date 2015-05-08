38591eb0a0cd3fe83655b8eef9f1fde72f1bd414
oAugus <auguscode@gmail.com>
2015-05-08 23:39:25 +0800

    Timeline Cell

---
+++ iAugus/TimelineCell.swift
@@ -0,0 +1,14 @@
+//
+//  TimelineCell.swift
+//  iAugus
+//
+//  Created by Augus on 5/8/15.
+//  Copyright (c) 2015 Augus. All rights reserved.
+//
+
+import UIKit
+
+class TimelineCell: UITableViewCell {
+
+    
+}
