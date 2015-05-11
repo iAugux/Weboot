6248125d687d6d1ffd1d6bf524abc8831df2b702
oAugus <auguscode@gmail.com>
2015-05-12 00:42:25 +0800

    set cell

---
+++ iAugus/OriginalWeiboTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  OriginalWeiboTableViewCell.swift
+//  iAugus
+//
+//  Created by Augus on 5/11/15.
+//  Copyright (c) 2015 Augus. All rights reserved.
+//
+
+import UIKit
+
+class OriginalWeiboTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
